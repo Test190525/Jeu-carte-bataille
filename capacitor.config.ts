c17b24f5071e4df224175f6645f22b6d7ea26658
Xiao Huli <test190525@gmail.com>
2026-07-30 18:44:39 +1100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bataille',
+  webDir: 'www'
+};
+
+export default config;
